e6d5bf3cf8ab07f199e394a364daf146559f370a
kelvinirungu <kelvinn.irungu69@gmail.com>
2023-11-20 17:10:21 +0300

    Change projects section background

--- src/components/work/Work.jsx
+++ src/components/work/Work.jsx
@@ -1,6 +1,6 @@
 function Work() {
   return (
-    <section id="work" className="pt-[60px]">
+    <section id="work" className="pt-[90px] back_round_color_home">
       <h2>Work</h2>
       <h2>Work</h2>
       <h2>Work</h2>
