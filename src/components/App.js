0e05eb9d73798effe908d4155f295906182696e5
kelvinirungu <kelvinn.irungu69@gmail.com>
2023-11-27 15:05:21 +0300

    Conditionally render loading screen

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,20 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { gsap } from 'gsap';
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Layout from './layout/Layout';
+import Loading from './loading/Loading';
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate loading for 2 seconds
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+  }, []);
+
   useEffect(() => {
     gsap.set('.ball', { xPercent: -50, yPercent: -50 });
     const targets = gsap.utils.toArray('.ball');
@@ -22,15 +32,19 @@ function App() {
   return (
     <>
       {/* <Layout /> */}
-      <div className="min-h-screen bg-black copy_selection ">
-        <div className="ball cursor_color_end cursor_color_styles w-6 h-6 fixed top-0 left-0 rounded-full" />
-        <div className="ball cursor_color_middle cursor_color_styles w-10 h-10 fixed top-0 left-0 rounded-full" />
-        <div className="ball cursor_color cursor_color_styles w-14 h-14 fixed top-0 left-0 rounded-full" />
-        <Routes>
-          <Route path="/" element={<Layout />} />
-        </Routes>
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <div className="min-h-screen bg-black copy_selection ">
+          <div className="ball cursor_color_end cursor_color_styles w-6 h-6 fixed top-0 left-0 rounded-full" />
+          <div className="ball cursor_color_middle cursor_color_styles w-10 h-10 fixed top-0 left-0 rounded-full" />
+          <div className="ball cursor_color cursor_color_styles w-14 h-14 fixed top-0 left-0 rounded-full" />
+          <Routes>
+            <Route path="/" element={<Layout />} />
+          </Routes>
+        </div>
+      )}
 
-      </div>
     </>
   );
 }
