3021a37c5c5287aaf428b63118e020de4d9528a9
kelvinirungu <kelvinn.irungu69@gmail.com>
2023-11-28 20:02:29 +0300

    loading page basic animation

--- src/components/loading/Loading.jsx
+++ src/components/loading/Loading.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import './loading.css';
 
 function Loading() {
   return (
-    <div className="loading">
-      <h1>Loading</h1>
-    </div>
+    <section className="back_round_color_home">
+      <div className="loading h-[100vh]">
+        <div className="loading_circle position_first" />
+        <div className="loading_circle position_second" />
+        <div className="loading_circle position_third" />
+        <div className="loading_animation_container absolute left-[5%] bottom-[10%] w-[50%]">
+          <p className="loading_text mt-[3%]">Loading</p>
+          <div className="loading_animation ml-[4%]" />
+          <div className="loading_animation" />
+          <div className="loading_animation" />
+        </div>
+      </div>
+    </section>
   );
 }
 
